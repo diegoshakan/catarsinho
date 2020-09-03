83df074b4f24b6a4c877b2f3f112ede360d6de5e
Diego Araujo <diego_shakan@hotmail.com>
2020-09-03 13:41:46 -0300

    fixed issue of endline date validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,11 +4,11 @@ class Project < ApplicationRecord
 
   has_many :donations, dependent: :destroy
 
-  validates :goal, :start_date, presence: true
+  validates :goal, :start_date, :endline, :image, presence: true
   validates :title, length: { maximum: 64 }, presence: true
   validates :description, length: { maximum: 512 }, presence: true
 
-  validate :limit_goal, :limit_start_date
+  validate :limit_goal, :limit_start_date, :date_to_endline
 
   def amount_per_cent
     (100 * self.amount_collected) / self.goal
@@ -24,6 +24,12 @@ class Project < ApplicationRecord
     def limit_start_date
       if (self.start_date < Date.current)
         errors.add(:start_date, "Data não pode ser anterior a hoje")
+      end
+    end
+
+    def date_to_endline
+      if (self.endline < Date.current || self.endline >= (Date.current + 30))
+        errors.add(:endline, "Precisa encerrar amanhã ou em até 30 dias")
       end
     end
 
--- db/migrate/20200902151950_add_amount_collected_to_projects.rb
+++ db/migrate/20200902151950_add_amount_collected_to_projects.rb
@@ -1,5 +1,5 @@
 class AddAmountCollectedToProjects < ActiveRecord::Migration[6.0]
   def change
-    add_column :projects, :amount_collected, :float
+    add_column :projects, :amount_collected, :float, default: 0
   end
 end
