ea272f8ff34933b92e13e9a7a273ccf70dbb844c
Diego Araujo <diego_shakan@hotmail.com>
2020-09-05 17:04:50 -0300

    send image to show in image

--- spec/requests/my_projects_request_spec.rb
+++ spec/requests/my_projects_request_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "MyProjects", type: :request do
+  let!(:project) { create(:project) }
+  let!(:user) { create(:user) }
 
   describe "GET /index" do
-    it "returns http success" do
-      get "/my_projects/index"
-      expect(response).to have_http_status(:success)
+    it "renders a successful response" do
+      project
+      get my_projects_index_path
+      expect(response).to be_successful
     end
   end
 
