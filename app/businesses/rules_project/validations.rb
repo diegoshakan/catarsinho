30c05d4e6f287e835ba67d7f0d31b9fef5bf7f86
Diego Araujo <diego_shakan@hotmail.com>
2021-03-07 18:01:16 -0300

    refac(project): remove business from model

--- app/businesses/rules_project/validations.rb
+++ app/businesses/rules_project/validations.rb
@@ -8,9 +8,21 @@ class RulesProject::Validations
     calculate_per_cent
   end
 
+  def unpermitted_goal
+    error_goal
+  end
+
   private
 
   def calculate_per_cent
     (100 * @project.amount_collected) / @project.goal
+  end
+
+  def validate_goal
+    @project.goal < 0 || @project.goal > 500
+  end
+
+  def error_goal
+    @project.errors[:base] << "Meta precisa estar entre 0 e 500" if validate_goal
   end
 end
