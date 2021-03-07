3072d17f2583a3e1493b786c01a64fc79940a719
Diego Araujo <diego_shakan@hotmail.com>
2021-03-07 18:14:36 -0300

    test(project): create test to unpermitted project start date before today

--- spec/businesses/rules_project/validations_spec.rb
+++ spec/businesses/rules_project/validations_spec.rb
@@ -6,31 +6,42 @@ RSpec.describe "RulesProject::Validations" do
     @project = FactoryBot.create(:project, goal: 200.00)
   end
 
-  context 'donation' do
-
-    it "per cent" do
-      @project.donations << FactoryBot.create(:donation, value_donation: 100.0)
-      @project.amount_collected = @project.donations.sum(:value_donation)
-
-      expect(@project.amount_per_cent).to eq(50)
-    end
-
-  end
-
   context 'project' do
 
-    it 'goal > 500' do
-      project = FactoryBot.create(:project)
-      project.goal = 550.0
+    context 'unpermitted' do
 
-      expect(project.valid?).to eq(false)
-    end
+      it 'goal > 500' do
+        project = FactoryBot.create(:project)
+        project.goal = 550.0
 
-    it 'goal < 500' do
-      project = FactoryBot.create(:project)
-      project.goal = -1.0
+        expect(project.valid?).to eq(false)
+      end
 
-      expect(project.valid?).to eq(false) #eq('ActiveRecord::RecordInvalid: Validation failed: Goal Meta precisa estar entre 0 e 500')
+      it 'goal < 500' do
+        project = FactoryBot.create(:project)
+        project.goal = -1.0
+
+        expect(project.valid?).to eq(false)
+      end
+
+      it '< today' do
+        project = FactoryBot.create(:project)
+        project.start_date = 2.days.ago
+
+        expect(project.valid?).to eq(false)
+      end
+
+      context 'donation' do
+
+        it "per cent" do
+          @project.donations << FactoryBot.create(:donation, value_donation: 100.0)
+          @project.amount_collected = @project.donations.sum(:value_donation)
+
+          expect(@project.amount_per_cent).to eq(50)
+        end
+
+      end
+
     end
 
   end
