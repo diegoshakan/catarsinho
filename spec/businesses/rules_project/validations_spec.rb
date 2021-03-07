79ed365633b741ed834df56ecbd76b35584c0baa
Diego Araujo <diego_shakan@hotmail.com>
2021-03-07 16:13:14 -0300

    done test and works =D

--- spec/businesses/rules_project/validations_spec.rb
+++ spec/businesses/rules_project/validations_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 RSpec.describe "RulesProject::Validations" do
 
   before do
-    @project = FactoryBot.create(:project, goal: 100.00)
+    @project = FactoryBot.create(:project, goal: 200.00)
   end
 
   context 'create' do
 
     it "build" do
-      @project.donations << FactoryBot.create(:donation, value_donation: 100.00)
+      @project.donations << FactoryBot.create(:donation, value_donation: 100.0)
+      @project.amount_collected = @project.donations.sum(:value_donation)
 
-      expect(@project.amount_per_cent).to eq(100)
+      expect(@project.amount_per_cent).to eq(50)
     end
 
   end
