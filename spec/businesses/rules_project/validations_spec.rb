24910ee592c90886a0e40571bc14e6a0db54a749
Diego Araujo <diego_shakan@hotmail.com>
2021-03-07 18:00:52 -0300

    test(project): create test to goal validates

--- spec/businesses/rules_project/validations_spec.rb
+++ spec/businesses/rules_project/validations_spec.rb
@@ -6,13 +6,31 @@ RSpec.describe "RulesProject::Validations" do
     @project = FactoryBot.create(:project, goal: 200.00)
   end
 
-  context 'create' do
+  context 'donation' do
 
-    it "build" do
+    it "per cent" do
       @project.donations << FactoryBot.create(:donation, value_donation: 100.0)
       @project.amount_collected = @project.donations.sum(:value_donation)
 
       expect(@project.amount_per_cent).to eq(50)
+    end
+
+  end
+
+  context 'project' do
+
+    it 'goal > 500' do
+      project = FactoryBot.create(:project)
+      project.goal = 550.0
+
+      expect(project.valid?).to eq(false)
+    end
+
+    it 'goal < 500' do
+      project = FactoryBot.create(:project)
+      project.goal = -1.0
+
+      expect(project.valid?).to eq(false) #eq('ActiveRecord::RecordInvalid: Validation failed: Goal Meta precisa estar entre 0 e 500')
     end
 
   end
