707a1b89000caef371d26b4db332ca36f5bd09da
Diego Araujo <diego_shakan@hotmail.com>
2021-03-10 22:35:55 -0300

    refac(donation): add busines in controller

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -5,9 +5,6 @@ FactoryBot.define do
     goal { 500 }
     start_date { Date.today }
     endline { Date.today + 15 }
-    # image { file_blob }
-    # image { "app/assets/images/noobcode.png" }
-    # image { definition_file_paths.(Rails.root.join('app', 'assets', 'images', 'noobcode.png')) }
     image { Rack::Test::UploadedFile.new('spec/fixtures/noobcode.png', 'image/png') }
     user
   end
