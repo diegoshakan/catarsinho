3ff025ef4caa23b2ecc0632785611567e61da7db
Diego Araujo <diego_shakan@hotmail.com>
2021-03-10 22:35:21 -0300

    refac(project): fixed validation

--- app/businesses/rules_project/validations.rb
+++ app/businesses/rules_project/validations.rb
@@ -26,29 +26,31 @@ class RulesProject::Validations
     (100 * @project.amount_collected) / @project.goal
   end
 
-  def validate_goal
-    @project.goal < 0 || @project.goal > 500
-  end
 
   def error_goal
     @project.errors[:base] << "Meta precisa estar entre 0 e 500" if validate_goal
   end
 
-  def validate_start_date
-    @project.start_date < Date.today
+  def validate_goal
+    @project.goal < 0 || @project.goal > 500
   end
 
   def error_date
     @project.errors[:base] << "Data não pode ser anterior a hoje" if validate_start_date
   end
 
-  def validate_endline
-    @project.endline <= Date.today || @project.endline >= Date.today + 30
+  def validate_start_date
+    unless @project.amount_collected_change
+      @project.start_date < Date.today
+    end
   end
 
   def error_endline
     @project.errors[:base] << "Precisa encerrar amanhã ou em até 30 dias" if validate_endline
   end
 
+  def validate_endline
+    @project.endline <= Date.today || @project.endline >= Date.today + 30
+  end
 
 end
